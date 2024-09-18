b78c135f74343f2f13fd47e8eed547d43fa98471
unknown <sfarkhan48@gmail.com>
2024-09-18 09:22:35 +0700

    notification

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,16 +1,18 @@
 import { curve, heroBackground, robot } from "../assets";
-import Section from "./Section";
 import Button from "./Button";
+import Section from "./Section";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constant";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import Generating from "./Generating";
+import Notification from "./Notification";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
+
   return (
-    <section
+    <Section
       className="pt-[12rem] -mt-[5.25rem]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
@@ -18,7 +20,7 @@ const Hero = () => {
       id="hero"
     >
       <div className="container relative" ref={parallaxRef}>
-        <div className="relative z-10 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
@@ -37,13 +39,14 @@ const Hero = () => {
             with Brainwave, the open AI chat app.
           </p>
           <Button href="/pricing" white>
-            Get Started
+            Get started
           </Button>
         </div>
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
-          <div className="relative z-10 p-1 rounded-2xl bg-conic-gradient">
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
               <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
                 <img
                   src={robot}
@@ -53,7 +56,7 @@ const Hero = () => {
                   alt="AI"
                 />
 
-                <Generating className="absolute left-1/2 bottom-8 md:w-[31rem] md:translate-x-[-50%]" />
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
@@ -64,12 +67,19 @@ const Hero = () => {
                     ))}
                   </ul>
                 </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
 
             <Gradient />
           </div>
-          <div className="absolute top-[50%] left-1/2 w-full -translate-x-1/2 -translate-y-1/2 md:w-[138%] lg:w-[100%]">
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
               src={heroBackground}
               className="w-full"
@@ -78,12 +88,13 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+
           <BackgroundCircles />
         </div>
       </div>
 
       <BottomLine />
-    </section>
+    </Section>
   );
 };
 
